16c81164add84d683272b0b5250a1e2f952933f1
takayuki takahashi <ttkaws15@gmail.com>
2019-10-31 08:23:28 +0900

    画像投稿機能作成/userとpicturesの紐付け

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,70 @@
+class PicturesController < ApplicationController
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to pictures_path, success: "ブログを作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @picture.update(picture_params)
+      redirect_to pictures_path, success: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        redirect_to pictures_path, success: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, danger:"ブログを削除しました！"
+  end
+
+  def confirm
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:content, :image, :image_cache)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :sessions
   resources :users
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
+
 end
