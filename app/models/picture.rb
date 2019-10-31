37ce17495289fbe8727502d08b191a215ede9c3a
takayuki takahashi <ttkaws15@gmail.com>
2019-10-31 15:32:06 +0900

    各ページcss調整

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :content, presence: true
+  validates :content, length: { in: 1..140 }
+  validates :image, presence: true
 end
