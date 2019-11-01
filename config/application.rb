06fb88abcfa60c1dd737792249d8ba592667c5f7
takayuki takahashi <ttkaws15@gmail.com>
2019-11-01 14:44:14 +0900

    Fix:更新ボタン/関連付け/headerリンク

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,13 @@ module FBClone
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
+
   end
 end
